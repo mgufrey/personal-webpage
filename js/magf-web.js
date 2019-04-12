319269e4e7e9d55fbfb817316d2d6e26e9f4ac9c
mgufrey <35777393+mgufrey@users.noreply.github.com>
2019-04-11 22:13:28 -0500

    some changes

--- js/magf-web.js
+++ js/magf-web.js
@@ -1,7 +1,14 @@
-	$(document).ready(function(){
+//	$(document).ready(function(){
 		
 
-		TweenLite.from(".skillset", 2, {opacity: 0, delay:2});
+		//TweenLite.from(".skillset", 2, {opacity: 0, delay:2});
 
-		TweenMax.staggerFrom([".main-txt h2",".my-name"], .85, {opacity:0, letterSpacing:"1em"}, 3);
-});
+		//TweenMax.staggerFrom([".main-txt h2",".my-name"], .85, {opacity:0, letterSpacing:".5em"}, 3);
+
+		//TweenLite.from(".picture-cont", 1.5,{marginTop:-500, opacity:0});
+		//TweenMax.staggerFrom(["",".content-cont h2",".content-cont p",".content-cont h3"], .85, {opacity:0}, 1.5);
+
+//});
+	//$(document).leave(function(){
+
+	//		});
